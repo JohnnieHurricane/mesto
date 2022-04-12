03c0f10c2a7c8e60bc9df9d35304526a84083180
Johnnie Hurricane <kroyen@ya.ru>
2022-04-12 10:03:55 +0300

    fix:Изменить размеры попапа

--- script/index.js
+++ script/index.js
@@ -1,5 +1,12 @@
+/*Объявляем переменные*/
 let popup = document.querySelector('.edit')
 let popupClose = document.querySelector('.popup__close')
 let inputName = document.querySelector('.popup__name')
 let inputProfession = document.querySelector('popup__profession')
 let submit = document.querySelector('.popup__save')
+let popupBtn = document.querySelector('.profile__edit-button')
+let addBtn  = document.querySelector('.profile__add-post')
+let like = document.querySelector('.elements__like')
+
+/*Вешаем слушатели*/
+popupBtn.addEventListener('click')
