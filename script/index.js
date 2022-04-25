1d78dcef3de7af4a8e220083add9c1d43fa048fe
Johnnie Hurricane <kroyen@ya.ru>
2022-04-25 18:10:54 +0300

    fix: Править попап, отступы, добавлять подчеркивание, выносить блоки в корень, править фон кнопок, править JS submitEvent

--- script/index.js
+++ script/index.js
@@ -1,45 +1,32 @@
-/*Объявляем переменные*/
-let popup = document.querySelector('.popup') // Находим форму в DOM
-let popupContainer = popup.querySelector('.popup__container')
-let popupClose = popup.querySelector('.popup__close')
-let inputName = popup.querySelector('.popup__name') // Находим поля формы в DOM
-let inputProfession = popup.querySelector('.popup__profession') // Находим поля формы в DOM
-let submit = popup.querySelector('.popup__save')
-let profile = document.querySelector('.profile')
+const popup = document.querySelector('.popup')
+const popupClose = popup.querySelector('.popup__close')
+let inputName = popup.querySelector('.popup__name') 
+let inputProfession = popup.querySelector('.popup__profession') 
+let changes = document.forms.popupForm;
+const profile = document.querySelector('.profile')
 let personName = profile.querySelector('.profile__name')
 let job = profile.querySelector('.profile__profession')
-let popupBtn = profile.querySelector('.profile__edit-button')
-let addBtn  = profile.querySelector('.profile__add-post')
-let elements = document.querySelector('.elements')
-let heart = elements.querySelector('.elements__like')
+const popupBtn = profile.querySelector('.profile__edit-button')
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function formSubmitHandler (evt) { //сохранить изменения
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Так мы можем определить свою логику отправки.
+    evt.preventDefault()
     personName.textContent = inputName.value
     job.textContent = inputProfession.value
-    // Получите значение полей jobInput и nameInput из свойства value
-    popup.classList.toggle('popup_isopen')
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
+    popup.classList.toggle('popup_opened')
 }
 
 function openPopup () { //открыть Попап
     inputName.value = personName.textContent    
     inputProfession.value = job.textContent
-    popup.classList.add('popup_isopen')
+    popup.classList.add('popup_opened')
 }
 
 function closePopup () { //закрыть Попап
-    popup.classList.remove('popup_isopen')
+    popup.classList.remove('popup_opened')
     inputName.value = personName.textContent    
     inputProfession.value = job.textContent
 }
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-submit.addEventListener('click', formSubmitHandler); 
+
+changes.addEventListener('submit', formSubmitHandler); 
 popupBtn.addEventListener('click', openPopup)
 popupClose.addEventListener('click', closePopup)
