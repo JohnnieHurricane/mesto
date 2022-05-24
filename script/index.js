6f40615070813bdce9ad47d9a02f0cae3f6c1035
Johnnie Hurricane <kroyen@ya.ru>
2022-05-24 10:16:49 +0300

    feature: Старт 5пр

--- script/index.js
+++ script/index.js
@@ -7,6 +7,32 @@ const profile = document.querySelector('.profile')
 let personName = profile.querySelector('.profile__name')
 let job = profile.querySelector('.profile__profession')
 const popupBtn = profile.querySelector('.profile__edit-button')
+const initialCards = [
+    {
+      name: 'Бендер с сигарой',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
 
 function formSubmitHandler (evt) { //сохранить изменения
     evt.preventDefault()
