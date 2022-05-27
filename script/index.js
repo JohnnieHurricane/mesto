f3a33e4ead0bf6bf6c8258248914bcec01d4f601
Johnnie Hurricane <kroyen@ya.ru>
2022-05-27 11:35:31 +0300

    feature: добавить попап новой карточки, переделать функции открыть/закрыть

--- script/index.js
+++ script/index.js
@@ -1,15 +1,18 @@
-const popup = document.querySelector('.popup')
-const popupClose = popup.querySelector('.popup__close')
-let inputName = popup.querySelector('.popup__name') 
-let inputProfession = popup.querySelector('.popup__profession') 
+const popupEditInfo = document.querySelector('.popup__place_profile')
+const addCardButton = document.querySelector('.profile__add-post')
+const newCard = document.querySelector('.popup__place_new-card')
+const popupCloseInfo = popupEditInfo.querySelector('.popup__close_place_info')
+const popupCloseCard = document.querySelector('.popup__close_place_new-card')
+let inputName = popupEditInfo.querySelector('.popup__name') 
+let inputProfession = popupEditInfo.querySelector('.popup__profession')
 let editProfileForm = document.forms.popupForm;
 const profile = document.querySelector('.profile')
 let personName = profile.querySelector('.profile__name')
 let job = profile.querySelector('.profile__profession')
-const popupBtn = profile.querySelector('.profile__edit-button')
+const popupEditUser = profile.querySelector('.profile__edit-button')
 const initialCards = [
     {
-      name: 'Бендер с сигарой',
+      name: 'Архыз',
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
     },
     {
@@ -33,26 +36,66 @@ const initialCards = [
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
   ];
+  
 
 function formSubmitHandler (evt) { //сохранить изменения
     evt.preventDefault()
     personName.textContent = inputName.value
     job.textContent = inputProfession.value
-    popup.classList.toggle('popup_opened')
+    popupEditInfo.classList.toggle('popup_opened')
 }
 
-function openPopup () { //открыть Попап
+function openPopupInfo () { //открыть Попап
     inputName.value = personName.textContent    
     inputProfession.value = job.textContent
-    popup.classList.add('popup_opened')
+    popupEditInfo.classList.add('popup_opened')
 }
 
-function closePopup () { //закрыть Попап
-    popup.classList.remove('popup_opened')
-    inputName.value = personName.textContent    
-    inputProfession.value = job.textContent
+function openPopupCard () {
+    newCard.classList.add('popup_opened')
+}
+
+function closePopupInfo () { //закрыть Попап  
+    popupEditInfo.classList.remove('popup_opened')
+}
+
+function closePopupCard () { //закрыть Попап  
+    popupCloseCard.classList.remove('popup_opened')
+}
+
+function cardSubmitHandler (evt) { //сохранить изменения
+    evt.preventDefault()    
+    newCard.classList.toggle('popup_opened')
+}
+
+function closePopupCard () {
+    newCard.classList.remove('popup_opened')
+}
+
+function handleAddCard() {    
+    openPopupCard()
 }
 
 editProfileForm.addEventListener('submit', formSubmitHandler); 
-popupBtn.addEventListener('click', openPopup)
-popupClose.addEventListener('click', closePopup)
+popupEditUser.addEventListener('click', openPopupInfo)
+popupCloseInfo.addEventListener('click', closePopupInfo)
+addCardButton.addEventListener('click', handleAddCard)
+popupCloseCard.addEventListener('click', closePopupCard)
+
+const cardsTable = document.querySelector(".elements__list")
+const cardsTemplate = document.querySelector(".cards__template")
+
+function renderCards() {
+    const getElement = initialCards.map(renderCard)
+    cardsTable.append(...getElement)
+}
+
+function renderCard(item) {
+    const elementFromTempale = cardsTemplate.content.cloneNode(true)
+    const cardTitle = elementFromTempale.querySelector(".elements__title")
+    cardTitle.textContent = item.name
+    elementFromTempale.querySelector(".elements__photo").src = item.link
+    return elementFromTempale
+}
+
+renderCards()
