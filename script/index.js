34ab79d705279466ea1fcb73a4c7daab9717a7a9
Johnnie Hurricane <kroyen@ya.ru>
2022-06-10 14:18:43 +0300

    rework: photo view handler

--- script/index.js
+++ script/index.js
@@ -1,18 +1,22 @@
-const popupEditInfo = document.querySelector('.popup__place_profile')
+const popupPlaceProfile = document.querySelector('.popup__place_profile')
 const addCardButton = document.querySelector('.profile__add-post')
-const newCard = document.querySelector('.popup__place_new-card')
-const popupCloseInfo = popupEditInfo.querySelector('.popup__close_place_info')
-const popupCloseCard = document.querySelector('.popup__close_place_new-card')
-const newCardTitle = document.querySelector('.popup__new-title')
-const newCardLink = document.querySelector('.popup__new-link')
-let inputName = popupEditInfo.querySelector('.popup__name')
-let inputProfession = popupEditInfo.querySelector('.popup__profession')
+const popupPlaceNewCard = document.querySelector('.popup__place_new-card')
+const popupCloseInfo = popupPlaceProfile.querySelector('.popup__close_place_info')
+const popupClosePlaceNewCard = document.querySelector('.popup__close_place_new-card')
+const viewPopup = document.querySelector('.popup__place_view')
+const popupCloseView = document.querySelector('.popup__close_place_view')
+const popupPlaceNewCardTitle = document.querySelector('.popup__new-title')
+const popupPlaceNewCardLink = document.querySelector('.popup__new-link')
+let inputName = popupPlaceProfile.querySelector('.popup__name')
+let inputProfession = popupPlaceProfile.querySelector('.popup__profession')
 let editProfileForm = document.forms.popupProfileForm
-let addNewCardForm = document.forms.popupCardForm
+let popupNewCardForm = document.forms.popupCardForm
 const profile = document.querySelector('.profile')
 let personName = profile.querySelector('.profile__name')
 let job = profile.querySelector('.profile__profession')
 const popupEditUser = profile.querySelector('.profile__edit-button')
+const cardsTable = document.querySelector(".elements__list")
+const cardsTemplate = document.querySelector(".cards__template")
 const initialCards = [
     {
         name: 'Архыз',
@@ -39,8 +43,6 @@ const initialCards = [
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
 ];
-const cardsTable = document.querySelector(".elements__list")
-const cardsTemplate = document.querySelector(".cards__template")
 
 function renderCards() {
     const getElement = initialCards.map(renderCard)
@@ -48,63 +50,85 @@ function renderCards() {
 }
 
 function renderCard(item) {
-    const elementFromTempale = cardsTemplate.content.cloneNode(true)
+    const elementFromTempale = cardsTemplate.content.cloneNode(true).querySelector('.elements__item')
     const cardTitle = elementFromTempale.querySelector(".elements__title")
-    const deleteCardButton = elementFromTempale.querySelector(".elements__delete-btn")
+    const deleteCardButton = elementFromTempale.querySelector(".elements__delete-btn")    
+    const like = elementFromTempale.querySelector('.elements__like')
+    const photo = elementFromTempale.querySelector('.elements__photo')
     cardTitle.textContent = item.name
     elementFromTempale.querySelector(".elements__photo").src = item.link
-    deleteCardButton.addEventListener('click', removeCard)
+    deleteCardButton.addEventListener('click', function removeCard(evt) {
+        const removeButton = evt.target.closest('.elements__item')
+        removeButton.remove()
+    })
+    like.addEventListener('click', function(evt) {
+        const likeButton = evt.target.closest('.elements__like')
+        likeButton.classList.add('elements__like_active')
+    })    
+    photo.addEventListener('click', function(evt) {
+        const photo = evt.target.closest('.elements__photo')
+        const cardElement = photo.closest('.elements__item')
+        const popupCardTitle = cardElement.querySelector('.elements__title')
+        const popupCardImage = cardElement.querySelector('.elements__photo')
+        const popupPlaceView = document.querySelector('.popup__place_view')
+        const viewCardImage = popupPlaceView.querySelector('.popup__card-image')
+        const viewCardTitle = popupPlaceView.querySelector('.popup__card-title')
+        viewCardImage.src = popupCardImage.src
+        viewCardTitle.textContent = popupCardTitle.textContent
+        popupPlaceView.classList.add('popup_opened')
+    })
     return elementFromTempale
 }
 
-function removeCard (evt) {
-    const removeButton = evt.target.closest('.elements__item')
-    removeButton.remove()
+function viewCardHandler() {
+    viewPopup.classList.add('.popup__opened')
 }
 
 function formSubmitHandler(evt) { //сохранить изменения
     evt.preventDefault()
     personName.textContent = inputName.value
     job.textContent = inputProfession.value
-    popupEditInfo.classList.toggle('popup_opened')
+    popupPlaceProfile.classList.toggle('popup_opened')
 }
 
 function openPopupInfo() { //открыть Попап
     inputName.value = personName.textContent
     inputProfession.value = job.textContent
-    popupEditInfo.classList.add('popup_opened')
+    popupPlaceProfile.classList.add('popup_opened')
 }
 
 function openPopupCard() {
-    newCard.classList.add('popup_opened')
+    popupPlaceNewCard.classList.add('popup_opened')
 }
 
 function closePopupInfo() { //закрыть Попап  
-    popupEditInfo.classList.remove('popup_opened')
+    popupPlaceProfile.classList.remove('popup_opened')
 }
 
-function closePopupCard() { //закрыть Попап  
-    popupCloseCard.classList.remove('popup_opened')
+function closePopupCard() {
+    popupPlaceNewCard.classList.remove('popup_opened')
+}
+
+function closePopupView() {
+    viewPopup.classList.remove('popup_opened')
 }
 
 function cardSubmitHandler(evt) {
     evt.preventDefault()
     const newTitleValue = document.querySelector('.popup__new-title').value
-    const newCardLinkValue = document.querySelector('.popup__new-link').value
-    const newTemplateCard = renderCard({ name: newTitleValue, link: newCardLinkValue })
+    const popupPlaceNewCardLinkValue = document.querySelector('.popup__new-link').value
+    const newTemplateCard = renderCard({ name: newTitleValue, link: popupPlaceNewCardLinkValue })
     cardsTable.prepend(newTemplateCard)
-    newCard.classList.toggle('popup_opened')
+    popupPlaceNewCard.classList.toggle('popup_opened')
+    popupNewCardForm.reset()
 }
 
-function closePopupCard() {
-    newCard.classList.remove('popup_opened')
-}
+renderCards()
 
 editProfileForm.addEventListener('submit', formSubmitHandler)
 popupEditUser.addEventListener('click', openPopupInfo)
 popupCloseInfo.addEventListener('click', closePopupInfo)
 addCardButton.addEventListener('click', openPopupCard)
-popupCloseCard.addEventListener('click', closePopupCard)
-addNewCardForm.addEventListener('submit', cardSubmitHandler)
-
-renderCards()
+popupClosePlaceNewCard.addEventListener('click', closePopupCard)
+popupNewCardForm.addEventListener('submit', cardSubmitHandler)
+popupCloseView.addEventListener('click', closePopupView)
