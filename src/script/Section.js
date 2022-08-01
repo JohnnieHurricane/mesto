0784012dad1c0e4070cdc5bad9e8692bfa9807fe
Johnnie Hurricane <kroyen@ya.ru>
2022-08-01 09:26:59 +0300

    feat: class section addeded

---
+++ src/script/Section.js
@@ -0,0 +1,17 @@
+export default class Section {
+    constructor({items, renderer}, containerSelector) {
+        this._items = items,
+        this._renderer= renderer,
+        this._container= document.querySelector(containerSelector)
+    }
+
+_renderer(item) {
+    this._container.append(item)
+}
+
+renderElements() {
+this._items.forEach(element) {
+    this._renderer(element)
+}
+}
+}
